087c00be6856906e7f74364b9054b6b6ed7444af
Dean Ellis <dellis1972@googlemail.com>
2023-07-25 13:27:48 +0100

    Add Windows DX Version

---
+++ Evolve.ChaseCamera.Windows/Program.cs
@@ -0,0 +1,24 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using System.Linq;
+#endregion
+
+namespace Evolve.ChaseCamera.Desktop
+{
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            var game = new ChaseCameraGame ();
+            game.Run();
+        }
+    }
+}
